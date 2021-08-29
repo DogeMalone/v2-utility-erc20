2767dbb1cd0f2e552b52d96add8da1a475620b67
Max Flow O2 <MaxflowO2@A22C6C6>
2021-08-29 17:36:33 +0000

    updated with sweep functions, talked about heavily in dev chats last night

--- contracts/DogeMalone.sol
+++ contracts/DogeMalone.sol
@@ -30,9 +30,53 @@ pragma solidity >= 0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DogeMalone is ERC20, ERC20Burnable {
+contract DogeMalone is ERC20, ERC20Burnable, Ownable {
   constructor() ERC20("DogeMalone", "DM") {
     _mint(msg.sender, 1000000000 * 10**18);
+  }
+
+  // @Dev if you renounce ownership any of these functions below go away
+  // these are sweep functions to remove any excess tokens sent to contract
+  // so you don't have to use the line of, "well it's on the contract, so
+  // can't do anything about it."
+
+  // @Dev function useful for accidental ETH transfers to contract (to owner)
+  // wraps owner() in payable to fix address -> address payable
+  function sweepEth() public onlyOwner {
+    payable(owner()).transfer(address(this).balance);
+  }
+
+  // @Dev function useful for accidental ETH transfers to contract (to user address)
+  // wraps _user in payable to fix address -> address payable
+  function sweepEthToAddress(address _user) public onlyOwner {
+    payable(_user).transfer(address(this).balance);
+  }
+
+  // @Dev function useful for accidental ERC20 transfers to contract (to owner)
+  // calls ERC20 tokens minted by this contract only, using IERC20.transfer()
+  function sweepTokens() public onlyOwner {
+    IERC20(address(this)).transfer(owner(), IERC20(address(this)).balanceOf(address(this)));
+  }
+
+  // @Dev function useful for accidental ERC20 transfers to contract (to user address)
+  // calls ERC20 tokens minted by this contract only, using IERC20.transfer()
+  function sweepTokensToAddress(address _user) public onlyOwner {
+    IERC20(address(this)).transfer(_user, IERC20(address(this)).balanceOf(address(this)));
+  }
+
+  // @Dev function useful for accidental ERC20 tokens of any type,
+  // transferred to contract (to owner). Calls any ERC20 tokens by address,
+  // and transfers via IERC20.transfer.
+  function sweepAnyTokens(address _token) public onlyOwner {
+    IERC20(_token).transfer(owner(), IERC20(_token).balanceOf(address(this)));
+  }
+
+  // @Dev function useful for accidental ERC20 tokens of any type,
+  // transferred to contract (to user address). Calls any ERC20 tokens by address,
+  // and transfers via IERC20.transfer.
+  function sweepAnyTokensToAddress(address _token, address _user) public onlyOwner {
+    IERC20(_token).transfer(_user, IERC20(_token).balanceOf(address(this)));
   }
 }
